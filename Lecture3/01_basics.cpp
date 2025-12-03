d30a117c9414da90efbe94c8537aadd8264950a2
Sofía Camacho <73445783+sofimaker@users.noreply.github.com>
2025-12-03 12:43:59 +0000

    lecture3

--- Lecture3/01_basics.cpp
+++ Lecture3/01_basics.cpp
@@ -7,16 +7,19 @@ int main() {
     std::cout << "Original value: " << secret_number << std::endl;
 
     // TODO 1: Print the memory address of 'secret_number' using the address-of operator (&)
-    // std::cout << "Address of secret: " << ... << std::endl;
+    std::cout << "Address of secret: " << &secret_number << std::endl;
 
 
     // TODO 2: Declare a pointer variable named 'ptr' (The Mailbox)
     // and initialize it to hold the address of 'secret_number'.
+
+    int* ptr = &secret_number;
     
 
     // TODO 3: Use the pointer 'ptr' to change the value of 'secret_number' to 999.
     // (Hint: You need to dereference it using *)
     
+    *ptr = 999;
 
     std::cout << "New value (should be 999): " << secret_number << std::endl;
 
--- Lecture3/02_modify.cpp
+++ Lecture3/02_modify.cpp
@@ -4,7 +4,10 @@
 // It should take an integer POINTER as a parameter.
 // It should multiply the value AT that address by 3.
 void tripler(int* number_ptr) {
-    // Your code here
+    
+    3* *number_ptr;
+    *number_ptr = 3* *number_ptr;
+    
     // Remember: *number_ptr gets you the value
 }
 
@@ -16,7 +19,8 @@ int main() {
     // TODO 2: Call the tripler function.
     // Hint: You cannot pass 'my_score' directly. You must pass its address!
     
-
+    tripler(&my_score);
+    //std::cout << "tripler value: " << tripler << std::endl;
     std::cout << "Score after (should be 30): " << my_score << std::endl;
 
     return 0;
